2211cc86545c597db42609466038e21260c56f80
Jekeee <glooffee@gmail.com>
2022-11-02 00:03:47 +0200

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { ProductService } from './services/product.service';
+import { Observable, tap } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'exchange-rates-angular';
+export class AppComponent implements OnInit {
+
+  title = 'Items list';
+
+  
+
+  products$: Observable<IProduct[]>
+
+  loading = false
+
+  term = ""
+
+  constructor(private productService  : ProductService){}
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productService.getAll().pipe(
+     tap(() => this.loading = false)
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {ProductComponent} from './components/product/product.component'
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GlobalErrorComponent } from './components/global-error/global-error.component';
+import { FormsModule } from '@angular/forms';
+import { FilterProductsPipe } from './pipes/filter-products.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    GlobalErrorComponent,
+    FilterProductsPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
